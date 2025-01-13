03c983e72c002e7eb9fc2dd96b17a258cf42248e
Amreet Khuntia <amreetleo123@gmail.com>
2025-01-13 06:49:51 +0530

    feat: added scroller for projects
    - added scoller for projects

--- src/lib/components/Projects/Projects.svelte
+++ src/lib/components/Projects/Projects.svelte
@@ -7,6 +7,8 @@
   import { projectStore } from "$stores/projects.ts";
 
   let source: Source = "github";
+  let activeIndex: number = 0;
+  let prevIndex: number = 0;
   $: projects = $projectStore.githubProjects ?? [];
 
   onMount(async () => {
@@ -19,8 +21,31 @@
   <Header hLevel={3}>
     <LazyLoader maxCount={1}>PROJECTS</LazyLoader>
   </Header>
+  <div class="carousel-dot-container">
+    <div class="display-flex display-flex-center">
+      {#each projects as _, index}
+        <!-- svelte-ignore a11y_click_events_have_key_events -->
+        <!-- svelte-ignore a11y_no_static_element_interactions -->
+        <div
+          class="carousel-dot-button {activeIndex === index
+            ? 'carousel-dot-button-active'
+            : ''}"
+          on:click={() => {
+            prevIndex = activeIndex;
+            activeIndex = index;
+          }}
+        ></div>
+      {/each}
+    </div>
+  </div>
   {#each projects as project, index}
-    <Project {source} {project} />
+    {#if activeIndex === index}
+      <Project
+        {source}
+        {project}
+        comeFrom={prevIndex > activeIndex ? "right" : "left"}
+      />
+    {/if}
   {/each}
 </div>
 
