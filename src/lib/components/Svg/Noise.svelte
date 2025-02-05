251c361d451fa29bd8393ef1c05620ff892b0b5d
Amreet Khuntia <amreetleo123@gmail.com>
2025-02-05 07:49:23 +0530

    feat: added noise
    - added filter noise

---
+++ src/lib/components/Svg/Noise.svelte
@@ -0,0 +1,10 @@
+<svg width="0" height="0">
+  <filter id="noiseFilter">
+    <feTurbulence
+      type="fractalNoise"
+      baseFrequency="0.65"
+      numOctaves="3"
+      stitchTiles="stitch"
+    />
+  </filter>
+</svg>
